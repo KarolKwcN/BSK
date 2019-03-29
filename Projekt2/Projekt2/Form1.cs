eb9dd09ca0de9a83f4e33ff2567da3047d75e3a8
Karol Kwiecień <karol.kwiecien91@gmail.com>
2019-03-29 23:13:30 +0100

    menu

--- Projekt2/Projekt2/Form1.cs
+++ Projekt2/Projekt2/Form1.cs
@@ -16,5 +16,15 @@ namespace Projekt2
         {
             InitializeComponent();
         }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
